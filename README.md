5bcf830c984a6a4d7225c64e2160dccefe07a5db
spline <reticulatingspline@github.com>
2013-02-17 23:07:49 -0500

    More updates..

--- README.md
+++ README.md
@@ -1,22 +1,41 @@
 Supybot-MegaHAL
 ===============
 
-* PLEASE NOTE THIS IS IN BETA/TESTING AND IS MOST LIKELY BUGGY. IT ALSO REQUIRES:
+Supybot plugin to make your bot talk. Think A.L.I.C.E. or MegaHal.
 
-cobe
+Warning/Disclaimer
 
-pip install cobe
+    I must warn you that with any plugin which makes a bot talk, you run a serious risk
+    of having the bot banned. Some people go *nuts* when bots talk not on command and
+    will ban/perm you and your bot. By default, I have the config registry variables
+    set to not talk very much, which can be changed. So, please, make sure that
+    you set each individual channel to be respectful of those who own it. Keep
+    the probability at 0 if you want the bot to never respond. Consider the code
+    in testing since something like this will always encounter bugs.
 
-* YOU WILL FIND BUGS IN THIS MOST LIKELY OR THE THING WILL GET YOUR BOT BANNED FROM A CHANNEL.
+Description
 
-AGAIN, THIS IS IN TESTING.
+    What bot is complete without some type of plugin to make it talk?
 
-IF YOU WOULD LIKE TO RUN/HELP/TEST, I appreciate any bug reports!
+Instructions
 
-Supybot plugin for MegaHAL to make your bot learn and speakInsert a description of your plugin here, with any notes, etc. about using it.
-http://madcow.googlecode.com/svn-history/r1271/trunk/madcow/modules/megahal.py
-http://teichman.org/blog/2011/02/cobe.html
-http://gozerplugs.googlecode.com/hg-history/03ccbb46d89f95705e47699281053200c3aaee67/megahal.py
-https://github.com/pteichman/kibot-modules/blob/master/kibot/modules/cobe.py
-https://github.com/Spacexplosion/Quaking-Mad-Cow/blob/9c22130ebfdc663666e495df8f81f61bb5fac24a/madcow/modules/megahal.py
-https://github.com/gsf/supybot-plugins/blob/1ccef4a72411bd14fb3115c520de1c002be8cbce/Supybot-plugins-20060723/Markov/plugin.py
+    You need cobe: pip install cobe
+
+    Description of cobe: http://teichman.org/blog/2011/02/cobe.html
+
+    After running my tests with 10+ channels for 1 week, my corpus is about 18Mb. I've not implemented
+    a way to labotomize it just yet. I will have to see how it performs when the corpus hits 100MB+
+
+    After that, the requirements are satisfied and this should work fine on Python 2.7+
+
+    I suggest you check out the channel config variables to tweak things like probability and delay.
+
+Inspiration/notes:
+
+    * http://madcow.googlecode.com/svn-history/r1271/trunk/madcow/modules/megahal.py
+    * http://gozerplugs.googlecode.com/hg-history/03ccbb46d89f95705e47699281053200c3aaee67/megahal.py
+    * https://github.com/pteichman/kibot-modules/blob/master/kibot/modules/cobe.py
+    * https://github.com/Spacexplosion/Quaking-Mad-Cow/blob/9c22130ebfdc663666e495df8f81f61bb5fac24a/madcow/modules/megahal.py
+    * https://github.com/gsf/supybot-plugins/blob/1ccef4a72411bd14fb3115c520de1c002be8cbce/Supybot-plugins-20060723/Markov/plugin.py
+    * https://github.com/bdrewery/PyBorg/blob/master/lib/pyborg/pyborg.py
+    * https://github.com/Trixarian/Alia/blob/master/pyborg.py
--- plugin.py
+++ plugin.py
@@ -9,10 +9,11 @@
 import time
 import re
 import random
-import string
+import os
 # extra supybot
 import supybot.ircmsgs as ircmsgs
 import supybot.conf as conf
+import supybot.schedule as schedule
 # stock supybot
 import supybot.utils as utils
 from supybot.commands import *
@@ -44,35 +45,40 @@ class MegaHAL(callbacks.Plugin):
     def die(self):
         self.__parent.die()
 
-    def _updateSentinel(self, channel):
+    def _updatesentinel(self, channel):
         now = time.time()
         try:
             lastreplied = self.last_replied[channel]
         except KeyError:
-            lastupdated = False
             self.last_replied[channel] = now
             self.log.debug("I have not spoke in {0} yet.".format(channel))
             return True
 
         if lastreplied:
             if now > (lastreplied + self.registryValue('waitTimeBetweenSpeaking', channel)):
-                self.log.debug("I spoke more than {0} ago in {1}".format(lastreplied,channel))
+                self.log.debug("I spoke more than {0} ago in {1}".format(lastreplied, channel))
                 self.last_replied[channel] = now
                 return True
             else:
-                self.log.debug("I spoke less than {0} ago in {1}".format(lastreplied,channel))
+                self.log.debug("I spoke less than {0} ago in {1}".format(lastreplied, channel))
                 return False
 
-    def _cleanText(self, text):
-        text = text.decode("utf-8")
+    def _cleantext(self, text):
+        """Clean-up text for input into corpus."""
+        text = text.decode("utf-8", 'ignore')
         text = ircutils.stripFormatting(text)
-        text = re.sub("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@.!?;:/%\$§\-_ ]", " ", text) # | re.UNICODE)
-        text = re.sub("[ ]+", " ", text)
         text = text.strip()
+        text = re.sub("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@.!?;:/%\$§\-_ ]", " ", text)
+        text = utils.str.normalizeWhitespace(text)
         return text
 
+    def removeNonAscii(self, s):
+        """Remove non-ascii characters."""
+
+        return "" . join(filter(lambda x: ord(x)<128, s))
+
     def _learn(self, irc, channel, text, probability):
-        text = self._cleanText(text)
+        text = self._cleantext(text)
         if text:
             if len(text) > 0 and not text.isspace():
                 b = Brain(self._brainfile)
@@ -80,7 +86,7 @@ class MegaHAL(callbacks.Plugin):
         # determine probability to respond.
         if random.randint(0, 100) < probability:
             # if we've randomly determined to talk, check the update time.
-            sentinel = self._updateSentinel(channel)
+            sentinel = self._updatesentinel(channel)
             if sentinel:
                 self._reply(irc, channel, text)
 
@@ -89,7 +95,12 @@ class MegaHAL(callbacks.Plugin):
         b = Brain(self._brainfile)
         response = b.reply(text).encode('utf-8')
         # delay the response here so we look real?
-        irc.queueMsg(ircmsgs.privmsg(channel, response))
+        if self.registryValue('responseDelay', channel):
+            self.log.info("Delayed response in %s" % channel)
+            delayseconds = time.time() + random.randint(2, 5)
+            schedule.addEvent((irc.queueMsg(ircmsgs.privmsg(channel, response))), delayseconds)
+        else:
+            irc.queueMsg(ircmsgs.privmsg(channel, response))
 
     def doPrivmsg(self, irc, msg):
         channel = msg.args[0].lower()
@@ -103,18 +114,43 @@ class MegaHAL(callbacks.Plugin):
             return
         # should we strip urls from text?
         if self.registryValue('stripUrls'):
-            text = re.sub('(http[^\s]*)', '', text)
+            text = re.sub(r'(http[^\s]*)', '', text)
         # determine probability if addressed or not.
         if callbacks.addressed(irc.nick, msg):
-            text = re.sub('(' + irc.nick + '*?\W+)', '', text)
+            text = re.sub('(' + irc.nick + '*?\W+)', '', text, re.IGNORECASE)
             probability = self.registryValue('probabilityWhenAddressed', channel)
         else:
             probability = self.registryValue('probability', channel)
+        # should we strip nicks from the text?
+        if self.registryValue('stripNicks'):
+            removenicks = '|'.join(item + '\W.*?\s' for item in irc.stats.channels[channel].users)
+            text = re.sub(r'' + removenicks + '', '', text)
         # finally, pass to our learn function.
         self._learn(irc, channel, text, probability)
 
+    def _prettysize(self, size):
+        suffixes = [("B",2**10), ("K",2**20), ("M",2**30), ("G",2**40), ("T",2**50)]
+        for suf, lim in suffixes:
+            if size > lim:
+                continue
+            else:
+                return round(size/float(lim/2**10),2).__str__()+suf
+
+    def corpussize(self, irc, msg, args):
+        """
+        Display the size of my brain on disk.
+        """
+
+        if os.path.exists(self._brainfile):
+            size = float(os.path.getsize(self._brainfile))
+            irc.reply("My brainfile is: {0}".format(self._prettysize(size)))
+        else:
+            irc.reply("ERROR: I am missing a brainfile.")
+
+    corpussize = wrap(corpussize)
+
     def corpuslearn(self, irc, msg, args, text):
-        text = self._cleanText(text)
+        text = self._cleantext(text)
         if text and len(text) > 1 and not text.isspace():
             irc.reply("I am learning: {0}".format(text))
             b = Brain(self._brainfile)
